commonquail/commitmsgfmt

commit faca080776d439aec97ef6917b10f7863f8cbf87

Break word iteration into its own module
This is merely preparation for non-breaking spaces.

Issue: https://gitlab.com/mkjeldsen/commitmsgfmt/-/issues/5

src/worditer.rs
@@ -1,0 +1,87 @@
+/// An iterator over "words" in some text. A word is generally a sequence of non-whitespace
+/// characters bounded by either whitespace characters or the empty string. However, this is not a
+/// definition and the exact details are unspecified.
+pub(crate) struct WordIter<'text> {
+    naive_words: core::str::Split<'text, char>,
+}
+
+impl<'text> WordIter<'text> {
+    pub fn new(text: &'text str) -> Self {
+        WordIter {
+            naive_words: text.split(' '),
+        }
+    }
+}
+
+impl<'text> Iterator for WordIter<'text> {
+    type Item = &'text str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.naive_words.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use pretty_assertions::assert_eq;
+
+    type Item<'text> = <WordIter<'text> as Iterator>::Item;
+
+    fn iter(text: &str) -> WordIter {
+        WordIter::new(&text)
+    }
+
+    fn collect(it: WordIter) -> Vec<Item> {
+        it.collect()
+    }
+
+    fn iter_collect(text: &str) -> Vec<Item> {
+        collect(iter(&text))
+    }
+
+    #[test]
+    fn smoke() {
+        let text = "a b #1 c [1] d";
+        let expect = ["a", "b", "#1", "c", "[1]", "d"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn empty_is_empty() {
+        let empty_string = "";
+        let expect = [empty_string];
+        let res = iter_collect(&empty_string);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn no_space_is_input() {
+        let no_space_text = "a";
+        let expect = ["a"];
+        let res = iter_collect(&no_space_text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn many_spaces() {
+        let text = "a   b";
+        let expect = ["a", "", "", "b"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn none_result_repeats() {
+        let res = {
+            let no_space_text = "a";
+            let mut it = iter(&no_space_text);
+            let _ = it.next();
+            [it.next(), it.next()]
+        };
+        let expect = [None, None];
+        assert_eq!(res, expect);
+    }
+}

src/commitmsgfmt.rs
@@ -1,6 +1,7 @@
 use crate::parser::parse;
 use crate::parser::Token;
 use crate::parser::Token::*;
+use crate::worditer::WordIter;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub struct CommitMsgFmt {
@@ -77,7 +78,7 @@ impl CommitMsgFmt {
             None => self.width,
         };
         let mut cur_line_len = 0;
-        for word in paragraph.split(' ') {
+        for word in WordIter::new(&paragraph) {
             let word_len = word.graphemes(true).count();
 
             // Not a new line so we need to fiddle with whitespace.

src/main.rs
@@ -11,6 +11,7 @@ use std::num::ParseIntError;
 
 mod commitmsgfmt;
 mod parser;
+mod worditer;
 
 type CliResult<T> = Result<T, CliError>;
 

commit 90826dab56984fa4e0bf104e1001fef29985855a

impl FusedIterator for WordIter
std::slice::Split has this and WordIter already behaves that way.

Fixes: faca080 (Break word iteration into its own module, 2021-04-04)
Fixes: 37da199 (Support non-breaking spaces for left-binding tokens, 2021-04-04)

src/worditer.rs
@@ -74,6 +74,8 @@ impl<'text> Iterator for WordIter<'text> {
     }
 }
 
+impl std::iter::FusedIterator for WordIter<'_> {}
+
 #[cfg(test)]
 mod tests {
     use super::*;