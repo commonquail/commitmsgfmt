commonquail/commitmsgfmt

commit 37da199476dd15f3d706b590d1c465df89ad85e7

Support non-breaking spaces for left-binding tokens
If a space-delimited token ("word") logically belongs with its preceding
word, consider these two words to be a single word and disallow wrapping
between them.

If a word begins with the current comment character, disallow wrapping
immediately before it to avoid the new line degenerating into a comment.

There is no support for tokens that should bind to the right, such as
indefinite articles. I was able to prove that this can be accomplished
but only with some very rough heuristics that I cannot yet justify.

Issue: https://gitlab.com/mkjeldsen/commitmsgfmt/-/issues/5

src/commitmsgfmt.rs
@@ -78,7 +78,7 @@ impl CommitMsgFmt {
             None => self.width,
         };
         let mut cur_line_len = 0;
-        for word in WordIter::new(&paragraph) {
+        for word in WordIter::new(&paragraph, self.comment_char) {
             let word_len = word.graphemes(true).count();
 
             // Not a new line so we need to fiddle with whitespace.
@@ -550,5 +550,31 @@ https://a.really-long-url.example
 [1] https://a.really-long-url.example
 ";
         assert_eq!(filter(10, &input), expected);
+    }
+
+    #[test]
+    fn does_not_break_non_breaking_words() {
+        let input = "
+foo
+
+foo #1 bar [1][qux] [2] baz -- wup
+
+[1] note
+[qux] note
+[2] note
+";
+        let expected = "
+foo
+
+foo #1
+bar [1][qux] [2]
+baz --
+wup
+
+[1] note
+[qux] note
+[2] note
+";
+        assert_eq!(filter(2, &input), expected);
     }
 }

src/worditer.rs
@@ -1,23 +1,76 @@
+use regex::Regex;
+
 /// An iterator over "words" in some text. A word is generally a sequence of non-whitespace
 /// characters bounded by either whitespace characters or the empty string. However, this is not a
-/// definition and the exact details are unspecified.
+/// definition and the exact details are unspecified. Rather, the iterator promises to produce
+/// results that wrap safely and sensibly.
 pub(crate) struct WordIter<'text> {
+    /// The commit message comment character, to avoid creating a word that starts with the comment
+    /// character lest that word degenerates into a comment.
+    comment_char: char,
+    /// The "next word", if present, may be considered the head of a list whose rest is
+    /// [`Self::naive_words`] before the next invocation of [`Self::next()`]. It was the last word
+    /// extracted from `naive_words` on the previous invocation of `next()` that was determined to
+    /// not be a logical part of the result of that invocation.
+    next_word: Option<&'text str>,
+    /// An iterator over each whitespace-separated token in the submitted text. `WordIter` may
+    /// consume multiple "naive words" to form a single "word" result during [`Self::next()`].
     naive_words: core::str::Split<'text, char>,
 }
 
+lazy_static! {
+    static ref FOOTNOTE_REFERENCE: Regex = Regex::new(r"^(?:\[[^]]+\])+$").unwrap();
+}
+
 impl<'text> WordIter<'text> {
-    pub fn new(text: &'text str) -> Self {
+    pub fn new(text: &'text str, comment_char: char) -> Self {
         WordIter {
+            comment_char,
+            next_word: None,
             naive_words: text.split(' '),
         }
+    }
+
+    fn is_non_breaking_word(&self, word: &str) -> bool {
+        word == "--" || word.starts_with(self.comment_char) || FOOTNOTE_REFERENCE.is_match(word)
     }
 }
 
 impl<'text> Iterator for WordIter<'text> {
-    type Item = &'text str;
+    type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.naive_words.next()
+        let word_start = self.next_word.or_else(|| self.naive_words.next());
+        if word_start.is_none() {
+            return None;
+        }
+
+        let word_start = word_start.unwrap();
+        let mut non_breaking_word = String::with_capacity(word_start.len() + 4);
+        non_breaking_word.push_str(word_start);
+
+        self.next_word = self.naive_words.next();
+        while let Some(word) = self.next_word {
+            if self.is_non_breaking_word(&word) {
+                non_breaking_word.push(' ');
+                non_breaking_word.push_str(word);
+            } else if word.is_empty() {
+                // str::split(' ') discards the matched space but produces empty
+                // strings instead; this is "surprising but intentional". We
+                // don't need those empty strings so skip them.
+                //
+                // split_whitespace() and split_ascii_whitespace() don't produce
+                // empty strings but match more tokens than we're used to; see
+                // be0833e (Document Unicode whitespace handling, 2018-09-16).
+                debug_assert!("a ".split_ascii_whitespace().collect::<Vec<&str>>() == vec!["a"]);
+            } else {
+                break;
+            }
+
+            self.next_word = self.naive_words.next();
+        }
+
+        Some(non_breaking_word)
     }
 }
 
@@ -26,11 +79,12 @@ mod tests {
     use super::*;
 
     use pretty_assertions::assert_eq;
+    use rand::seq::SliceRandom;
 
     type Item<'text> = <WordIter<'text> as Iterator>::Item;
 
     fn iter(text: &str) -> WordIter {
-        WordIter::new(&text)
+        WordIter::new(&text, '#')
     }
 
     fn collect(it: WordIter) -> Vec<Item> {
@@ -41,10 +95,23 @@ mod tests {
         collect(iter(&text))
     }
 
+    fn some_comment_char() -> char {
+        let some_comment_chars = ['#', ';', '!', '%'];
+        *some_comment_chars.choose(&mut rand::thread_rng()).unwrap()
+    }
+
     #[test]
     fn smoke() {
         let text = "a b #1 c [1] d";
-        let expect = ["a", "b", "#1", "c", "[1]", "d"];
+        let expect = ["a", "b #1", "c [1]", "d"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn smoke_ridiculous() {
+        let text = "a #1 #2 b [1][2] [3] [4]c d";
+        let expect = ["a #1 #2", "b [1][2] [3]", "[4]c", "d"];
         let res = iter_collect(&text);
         assert_eq!(res, expect);
     }
@@ -68,7 +135,7 @@ mod tests {
     #[test]
     fn many_spaces() {
         let text = "a   b";
-        let expect = ["a", "", "", "b"];
+        let expect = ["a", "b"];
         let res = iter_collect(&text);
         assert_eq!(res, expect);
     }
@@ -82,6 +149,73 @@ mod tests {
             [it.next(), it.next()]
         };
         let expect = [None, None];
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn merges_comment_char() {
+        let comment_char = some_comment_char();
+
+        let res = {
+            let text = format!("a {}1b d", comment_char);
+            let it = WordIter::new(&text, comment_char);
+            collect(it)
+        };
+        let expect = [&format!("a {}1b", comment_char), "d"];
+
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn merges_en_dash() {
+        let text = "a -- b";
+        let expect = ["a --", "b"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn lone_comment_char_binds_left() {
+        // This is a limitation of the text analysis heuristic. The test case is
+        // a special-case of "merges_comment_char()" made explicit.
+        //
+        // The first space after a comment character marks the end of a word,
+        // that comment character being the last part of the word.
+        // This means that "a #1" and "a # 1" behave differently, producing
+        // 1 respectively 2 words, the comment character always in the same word
+        // as the preceding token:
+        // - The comment character must join the preceding token to avoid being
+        //   pushed onto its own line and accidentally degrading into a comment.
+        // - The token after the comment character cannot join the the preceding
+        //   token because we have no way to determine that that token or any of
+        //   the subsequent tokens should be individual words or parts of the
+        //   first token -- this is the least surprising heuristic we can apply.
+        let comment_char = some_comment_char();
+
+        let res = {
+            let text = format!("a {} b", comment_char);
+            let it = WordIter::new(&text, comment_char);
+            collect(it)
+        };
+        let expect = [&format!("a {}", comment_char), "b"];
+
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn merges_footnote_number_references() {
+        let some_num = rand::random::<u8>();
+        let text = format!("a [{0}] [{0}] b", some_num);
+        let expect = [&format!("a [{0}] [{0}]", some_num), "b"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn merges_footnote_text_references() {
+        let text = "a [foo] [bar] b";
+        let expect = ["a [foo] [bar]", "b"];
+        let res = iter_collect(&text);
         assert_eq!(res, expect);
     }
 }

Cargo.toml
@@ -15,6 +15,8 @@ regex = "1.4.4"
 unicode-segmentation = "1.7.1"
 clap = "2.33.3"
 encoding = "0.2.33"
+lazy_static = "1.4.0"
 
 [dev-dependencies]
 pretty_assertions = "0.7.1"
+rand = "0.8.3"

src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 use clap::crate_description;
 use clap::crate_name;
 use clap::crate_version;

commit 7c03542042249b8b0a4ae3dcd34fb3ec9b02c397

Fix wrapping of footnote references followed by punctuation
When a footnote reference is immediately followed by punctuation, that
reference will not be merged with the preceding word when wrapping, as
it would have been without punctuation. That is,

    foo [1]
    bar [2].

can mistakenly degenerate into

    foo [1]
    bar
    [2].

This commit resolves that by accepting any unbroken sequence of
arbitrary punctuation on the right as part of that same word. The issue
"persists" where at least one space separates the reference and the
start of the punctuation sequence but we take the spacing as a sign of
deliberate intent and allow that.

Fixes: 37da199 (Support non-breaking spaces for left-binding tokens, 2021-04-04)

src/commitmsgfmt.rs
@@ -222,6 +222,7 @@ foo
         let msg = "
 foo
 
+paragraph
 [2] note
 [1] note
 [3]    foo bar baz qux https://a.really-long-url.example
@@ -234,6 +235,7 @@ foo
         let expected = "
 foo
 
+paragraph
 [2] note
 [1] note
 [3] foo bar baz qux
@@ -249,6 +251,38 @@ foo
 ";
 
         assert_eq!(filter(20, &msg), expected);
+    }
+
+    #[test]
+    fn formats_footnote_references() {
+        let msg = "
+foo
+
+x [1] x
+[1].
+
+yy [2] y
+[2] .
+
+zzz [3]
+z
+[3] .
+";
+        let expected = "
+foo
+
+x [1]
+x [1].
+
+yy [2] y
+[2] .
+
+zzz [3]
+z
+[3] .
+";
+
+        assert_eq!(filter(8, &msg), expected);
     }
 
     #[test]

src/parser.rs
@@ -564,6 +564,29 @@ subject
     }
 
     #[test]
+    fn footnote_does_not_join_immediately_preceding_paragraph() {
+        assert_eq!(
+            parse(
+                "
+subject
+
+paragraph
+[1] footnote 1
+[2] footnote 2
+"
+            ),
+            [
+                VerticalSpace,
+                Subject("subject".to_owned()),
+                VerticalSpace,
+                Paragraph("paragraph".to_owned()),
+                Footnote("[1]".to_owned(), "footnote 1".to_owned()),
+                Footnote("[2]".to_owned(), "footnote 2".to_owned()),
+            ],
+        );
+    }
+
+    #[test]
     fn footnote_order_is_unchanged() {
         // Naive solution is technically trivial but may not match semantics.
         assert_eq!(

src/worditer.rs
@@ -20,7 +20,17 @@ pub(crate) struct WordIter<'text> {
 }
 
 lazy_static! {
-    static ref FOOTNOTE_REFERENCE: Regex = Regex::new(r"^(?:\[[^]]+\])+$").unwrap();
+    static ref FOOTNOTE_REFERENCE: Regex = Regex::new(
+        r"(?x)
+        ^
+        # One or more unseparated '[any]' tokens...
+        (?:\[[^]]+\])+
+        # ... optionally immediately followed by any sort of punctuation, for
+        # example in case this is the end of the sentence.
+        (?:\p{Punctuation}*)
+        $"
+    )
+    .unwrap();
 }
 
 impl<'text> WordIter<'text> {
@@ -108,8 +118,8 @@ mod tests {
 
     #[test]
     fn smoke_ridiculous() {
-        let text = "a #1 #2 b [1][2] [3] [4]c d";
-        let expect = ["a #1 #2", "b [1][2] [3]", "[4]c", "d"];
+        let text = "a #1 #2 b [1][2] [3]. [4]c d";
+        let expect = ["a #1 #2", "b [1][2] [3].", "[4]c", "d"];
         let res = iter_collect(&text);
         assert_eq!(res, expect);
     }
@@ -184,7 +194,7 @@ mod tests {
         // as the preceding token:
         // - The comment character must join the preceding token to avoid being
         //   pushed onto its own line and accidentally degrading into a comment.
-        // - The token after the comment character cannot join the the preceding
+        // - The token after the comment character cannot join the preceding
         //   token because we have no way to determine that that token or any of
         //   the subsequent tokens should be individual words or parts of the
         //   first token -- this is the least surprising heuristic we can apply.
@@ -213,6 +223,23 @@ mod tests {
     fn merges_footnote_text_references() {
         let text = "a [foo] [bar] b";
         let expect = ["a [foo] [bar]", "b"];
+        let res = iter_collect(&text);
+        assert_eq!(res, expect);
+    }
+
+    #[test]
+    fn allows_punctuation_after_footnote_references() {
+        let text = "a [foo]. b [bar].:#,;![]() [x] c [baz] .# d [qux]   ... e [qaz]f";
+        let expect = [
+            "a [foo].",
+            "b [bar].:#,;![]() [x]",
+            "c [baz]",
+            ".#",
+            "d [qux]",
+            "...",
+            "e",
+            "[qaz]f",
+        ];
         let res = iter_collect(&text);
         assert_eq!(res, expect);
     }